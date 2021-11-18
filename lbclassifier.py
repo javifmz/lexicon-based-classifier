be11d17b29b8f6d69264752c960bff931fb53fb6
javifmz <45376469+javifmz@users.noreply.github.com>
2021-11-18 11:25:18 +0000

    Add prediction with labels and probabilites. Remove fit method (only predictor).

--- lbclassifier.py
+++ lbclassifier.py
@@ -10,31 +10,61 @@ class LexiconBasedClassifier :
     self.labels = labels.copy() if labels is not None else [ term for term in lexicon ]
     self.labels_index = { term: index for index, term in enumerate(self.labels) }
 
-  def fit(self, X) :
-    """This method does nothing, only for compatibility"""
-    pass
+  def _get_single_weights(self, raw_document) :
+    doc_tokens = set(self.tokenizer(raw_document))
+    doc_weight = [0] * len(self.lexicon)
+    for label, label_lexicon in self.lexicon.items() :
+      label_index = self.labels_index[label]
+      for term_data in label_lexicon :
+        term = term_data['term']
+        term_weight = term_data['weight']
+        term_tokens = self.tokenizer(term)
+        if term_tokens[0] in doc_tokens :
+          doc_weight[label_index] += term_weight
+    return doc_weight
 
-  def predict(self, X) :
-    predict = [0] * len(X)
-    for doc_index, doc in enumerate(X) :
-      doc_tokens = set(self.tokenizer(doc))
-      doc_weight = [0] * len(self.lexicon)
-      for label, label_lexicon in self.lexicon.items() :
-        label_index = self.labels_index[label]
-        for term_data in label_lexicon :
-          term = term_data['term']
-          term_weight = term_data['weight']
-          term_tokens = self.tokenizer(term)
-          if term_tokens[0] in doc_tokens :
-            doc_weight[label_index] += term_weight
-      max_value = max(doc_weight)
-      max_index = doc_weight.index(max_value)
-      predict[doc_index] = max_index
-    return predict
+  def predict_single(self, raw_document) :
+    doc_weight = self._get_single_weights(raw_document)
+    max_value = max(doc_weight)
+    max_index = doc_weight.index(max_value)
+    return max_index
   
-  def predict_labels(self, X) :
-    prediction = self.predict(X)
+  def predict_proba_single(self, raw_document) :    
+    doc_weight = self._get_single_weights(raw_document)
+    total = sum(doc_weight)
+    doc_proba = [ weight / total for weight in doc_weight ] if total > 0 else [0] * len(doc_weight)
+    return doc_proba
+
+  def predict_single_label(self, raw_document) :
+    label_index = self.predict_single(raw_document)
+    return self.labels[label_index]
+
+  def predict_proba_single_label(self, raw_document) :
+    doc_proba = self.predict_proba_single(raw_document)
+    return { self.labels[label_index]: proba for label_index, proba in enumerate(doc_proba) }
+
+  def predict(self, raw_documents) :
+    predict = [0] * len(raw_documents)
+    for index, raw_document in enumerate(raw_documents) :
+      predict[index] = self.predict_single(raw_document)
+    return predict
+
+  def predict_proba(self, raw_documents) :
+    predict = [None] * len(raw_documents)
+    for index, raw_document in enumerate(raw_documents) :
+      predict[index] = self.predict_proba_single(raw_document)
+    return predict
+
+  def predict_labels(self, raw_documents) :
+    prediction = self.predict(raw_documents)
     return self.get_labels(prediction)
+
+  def predict_proba_labels(self, raw_documents) :
+    prediction = self.predict_proba(raw_documents)
+    return self.get_proba_labels(prediction)
 
   def get_labels(self, prediction) :
     return [ self.labels[label_index] for label_index in prediction ]
+  
+  def get_proba_labels(self, prediction_proba) :
+    return [ { self.labels[label_index]: proba for label_index, proba in enumerate(doc_proba) } for doc_proba in prediction_proba ]
--- lbclassifier_example.py
+++ lbclassifier_example.py
@@ -1,6 +1,7 @@
 
 from lbclassifier import LexiconBasedClassifier
 
+# Lexicon
 lexicon = {
   "positive": [
     { "term": "excellent", "weight": 1.0 },
@@ -12,14 +13,46 @@ lexicon = {
   ],
 }
 
-X = [
+# Documents to classify
+raw_documents = [
   'This book is excellent',
   'This book is horrible',
 ]
 
+# Ordered labels (only necessary if the order is different from the lexicon)
 labels = [ "positive", "negative" ]
 
+# Create the classifier
 classifier = LexiconBasedClassifier(lexicon, labels=labels)
-prediction = classifier.predict(X)
-prediction_labels = classifier.get_labels(prediction)
-print(prediction, prediction_labels)
+
+# Predict the labels index of a dataset
+predict = classifier.predict(raw_documents)
+print(predict)
+
+# Predict the labels of a dataset
+predict_labels = classifier.predict_labels(raw_documents)
+print(predict_labels)
+
+# Predict with probabilities for a dataset
+predict_proba = classifier.predict_proba(raw_documents)
+print(predict_proba)
+
+# Predict with labels and probabilities for a dataset
+predict_proba_labels = classifier.predict_proba_labels(raw_documents)
+print(predict_proba_labels)
+
+# Predict the label index of a single instance
+predict_single = classifier.predict_single(raw_documents[0])
+print(predict_single)
+
+# Predict the label of a single instance
+predict_single_label = classifier.predict_single_label(raw_documents[0])
+print(predict_single_label)
+
+# Predict with probabilities for a single instance
+predict_proba_single = classifier.predict_proba_single(raw_documents[0])
+print(predict_proba_single)
+
+# Predict with labels and probabilities for a single instance
+predict_proba_single_label = classifier.predict_proba_single_label(raw_documents[0])
+print(predict_proba_single_label)
